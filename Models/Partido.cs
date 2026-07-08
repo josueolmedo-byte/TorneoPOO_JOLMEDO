054cdc5dc1c657918c509ac8e204b6c94bae1b31
Josué Olmedo <josue.olmedo@tecnologicoliceocristiano.edu.ec>
2026-07-07 23:15:19 -0500

    Se agregó un constructor a la clase Partido y se eliminó el método Programar. Además, se modificó la creación del objeto Partido en Program.cs para utilizar el constructor.

--- Models/Partido.cs
+++ Models/Partido.cs
@@ -14,18 +14,21 @@ namespace TorneoPOO_EMANOSALVAS.Models
         public DateTime Fecha { get; set; }
         public string Lugar { get; set; }
 
-        public void Programar(Equipo local, Equipo visitante, DateTime fecha, string lugar)
+        public Partido(Equipo local, Equipo visitante, DateTime fecha, string lugar)
         {
             this.Local = local;
             this.Visitante = visitante;
             this.Fecha = fecha;
             this.Lugar = lugar;
-            Console.WriteLine("Partido programado correctamente");
         }
 
         public void MostrarResumen()
         {
             Console.WriteLine($"Hay un partido programado entre el local {this.Local.Nombre} y el visitante {this.Visitante.Nombre} en el lugar {this.Lugar}");
+        }
+        public void RegistrarResultado(int golesLocal, int golesVisitante)
+        {
+            Console.WriteLine($"{Local.Nombre} {golesLocal} - {golesVisitante} {Visitante.Nombre}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,33 +1,37 @@
 ﻿using TorneoPOO_EMANOSALVAS.Models;
 using TorneoPOO_JOLMEDO.Models;
-
+//Clase Jugador
 Jugador objJugador1 = new Jugador("Lamine Yamal", 20, 19, "Extremo Derecho");
 
 Jugador objJugador2 = new Jugador("Lionel Messi", 39, 10, "Delantero");
 
-
+//Clase equipo
 Equipo objEquipo1 = new Equipo("Barcelona FC", "Barcelona");
 
 objEquipo1.AgregarJugador(objJugador1);
 objEquipo1.AgregarJugador(objJugador2);
 
-
 objEquipo1.ListarPlantilla();
 
+//Clase Jugador
 Jugador objJugador3 = new Jugador("Erling Haaland", 25, 9, "Delantero");
 
 Jugador objJugador4 = new Jugador("Mikel Oyarzabal", 29, 21, "Delantero");
 
-
+//Clase Equipo
 Equipo objEquipo2 = new Equipo("Manchester City", "Manchester");
 
 objEquipo2.AgregarJugador(objJugador3);
 objEquipo2.AgregarJugador(objJugador4);
 
-
-
 objEquipo2.ListarPlantilla();
 
-Partido objPartido1 = new Partido();
-objPartido1.Programar(objEquipo1, objEquipo2, DateTime.Now, "Manchester");
+//Clase Partido
+Partido objPartido1 = new Partido(objEquipo1, objEquipo2, DateTime.Now, "Barcelona");
 objPartido1.MostrarResumen();
+
+objJugador1.Entrenar();
+
+objEquipo1.BuscarJugadorPorNumero(10);
+
+objPartido1.RegistrarResultado(3, 1);
